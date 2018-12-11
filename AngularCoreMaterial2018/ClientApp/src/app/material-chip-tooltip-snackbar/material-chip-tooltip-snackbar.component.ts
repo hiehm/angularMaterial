71e03c7ee5017337b4cde60c138a708c42ebb0d8
mattchen0512 <mattchen0512@gmail.com>
2018-12-11 14:47:51 +0800

    Add: Chip Fix: 修正Azure 兩次發佈後造成Path路徑不一致問題 outputPath "dist/xxx" -> "./dist/xxx"

--- AngularCoreMaterial2018/ClientApp/src/app/material-chip-tooltip-snackbar/material-chip-tooltip-snackbar.component.ts
+++ AngularCoreMaterial2018/ClientApp/src/app/material-chip-tooltip-snackbar/material-chip-tooltip-snackbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+
 
 @Component({
   selector: 'app-material-chip-tooltip-snackbar',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./material-chip-tooltip-snackbar.component.css']
 })
 export class MaterialChipTooltipSnackbarComponent implements OnInit {
-
+  tags: any[];
+  separatorKeysCodes: any[];
   constructor() { }
 
   ngOnInit() {
+    this.tags = ['MATT', 'MARY', 'ILANDY'];
+    this.separatorKeysCodes = [ENTER, COMMA];
   }
-
+  addTag($event: MatChipInputEvent) {
+    if (($event.value || '').trim()) {
+      const value = $event.value.trim();
+      //判斷是否已存於Array之中
+      if (this.tags.indexOf(value) === -1) {
+        this.tags.push(value);
+      }
+    }
+    //清空輸入值
+    $event.input.value = '';
+  }
+  removeTag(tagName) {
+    console.log(tagName);
+    this.tags = this.tags.filter(tag => tag !== tagName);
+  }
 }
